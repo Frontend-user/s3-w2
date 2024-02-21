08ea06baba0ac121d98632d98dbec2dd228da985
Robert <robersargsyan2023@gmail.com>
2024-02-22 01:55:16 +0300

    deploy

--- src/users/repository/users-repository.ts
+++ src/users/repository/users-repository.ts
@@ -1,14 +1,16 @@
 import {UserCreateType, UserEmailEntityType, UserHashType} from "../types/user-types";
 import {UserModel, usersCollection} from "../../db";
 import {ObjectId} from "mongodb";
+import {response} from "express";
 
 export const usersRepositories = {
 
     async createUser(user: UserEmailEntityType): Promise<false | ObjectId> {
-        // const response =  await usersCollection.insertOne(user)
-        const response = new UserModel(user)
-        await response.save()
-        return response ? response._id : false
+        const response =  await usersCollection.insertOne(user)
+        // const response = new UserModel(user)
+        // await response.save()
+        // return response ? response._id : false
+        return response.insertedId
     },
     async deleteUser(id: ObjectId) {
         const response = await UserModel.deleteOne({_id: id})
